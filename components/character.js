107561be9e2ee657540b513ebfa65d2560b2bae9
jadecss <ttjit963@gmail.com>
2022-02-05 01:38:52 +0000

    fix

--- components/character.js
+++ components/character.js
@@ -6,7 +6,7 @@ const Character = ({ name, status, species, gender, image }) => {
 
     return (
         <button className="text-gray-500 font-semibold py-1 px-2 pr-4 rounded-full inline-flex items-center">
-            <Image className="fill-current rounded-full" src={image} width={32} height={32}/>
+            <Image className="fill-current rounded-full" alt='' src={image} width={32} height={32}/>
             <span className='ml-2'>{displayName}</span>
         </button>
     )
